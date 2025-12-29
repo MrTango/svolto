979a87ae7f7fb0024398fb6c2b1405f985b7bb5b
MrTango <md@derico.de>
2025-12-29 23:54:31 +0200

    improve devcontainer setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		proxy: {
+			'/Plone': {
+				target: 'http://backend:8080',
+				changeOrigin: true
+			}
+		}
+	},
 	test: {
 		workspace: [
 			{
